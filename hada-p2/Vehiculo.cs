b70e2b9236cd7f9a604484493dd3ddc4c8654740
Rafa <rvlc1@alu.ua.es>
2020-02-26 17:51:01 +0100

    48787742G Añadido Vehiculo.cs con propiedades publicas-staticas

---
+++ hada-p2/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Vehiculo
+    {
+        public static int maxVelocidad;
+        public static int maxTemperatura;
+        public static int minCombustible;
+        public static Random rand;
+    
+    }
+}
