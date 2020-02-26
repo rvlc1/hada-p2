65b3bb9423105cc28a6d13e8b9ba92b6ec6d9c18
Rafa <rvlc1@alu.ua.es>
2020-02-26 18:16:10 +0100

    48787742G Añadido get y set a publicas-staticas y las propiedades private-int

--- hada-p2/Vehiculo.cs
+++ hada-p2/Vehiculo.cs
@@ -8,10 +8,16 @@ namespace Hada
 {
     class Vehiculo
     {
-        public static int maxVelocidad;
-        public static int maxTemperatura;
-        public static int minCombustible;
-        public static Random rand;
-    
+        public static int maxVelocidad { get; set; }
+        public static int maxTemperatura { get; set; }
+        public static int minCombustible { get; set; }
+        public static Random rand
+        {
+            set; private get;
+        }
+        public string nombre { get; private set; }
+        private int velocidad;
+        private int temperatura;
+        private int combustible;
     }
 }
